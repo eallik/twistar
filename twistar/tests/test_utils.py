a7320abe1c381946d434f90f73b5eb037a15d09c
Erik Allik <eallik@gmail.com>
2012-11-24 06:54:58 +0200

    Made test_utils PEP8 compatible

--- twistar/tests/test_utils.py
+++ twistar/tests/test_utils.py
@@ -1,24 +1,22 @@
 from twisted.trial import unittest
-from twisted.enterprise import adbapi
 from twisted.internet.defer import inlineCallbacks
 
 from twistar import utils
-from twistar.registry import Registry
 
-from utils import *
+from .utils import User, initDB, tearDownDB
+
 
 class UtilsTest(unittest.TestCase):
-    
+
     @inlineCallbacks
     def setUp(self):
         yield initDB(self)
         self.user = yield User(first_name="First", last_name="Last", age=10).save()
 
-
     @inlineCallbacks
     def test_joinWheres_precedence(self):
         yield User(first_name="Second").save()
-        
+
         first = ['first_name = ?', "First"]
         last = ['last_name = ?', "Last"]
         second = ['first_name = ?', "Second"]
@@ -29,17 +27,14 @@ class UtilsTest(unittest.TestCase):
         results = yield User.count(where=where)
         self.assertEqual(1, results)
 
-
     def test_joinMultipleWheres_empty_arg(self):
         where = utils.joinMultipleWheres([], joiner='AND')
         self.assertEqual(where, [])
-    
 
     def test_joinMultipleWheres_single_where(self):
         where = ['first_name = ?', "First"]
         joined_where = utils.joinMultipleWheres([where], joiner='AND')
         self.assertEqual(where, joined_where)
-
 
     @inlineCallbacks
     def test_joinMultipleWheres(self):
@@ -54,8 +49,6 @@ class UtilsTest(unittest.TestCase):
         results = yield User.count(where=where)
         self.assertEqual(1, results)
 
-
     @inlineCallbacks
     def tearDown(self):
         yield tearDownDB(self)
-
